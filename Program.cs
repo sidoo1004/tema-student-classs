e47d39d176d02c85f9c31ae8b14e47fefd71e62d
adrian sidorencu <sidorencuadi6@gmail.com>
2022-01-11 16:06:20 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StudentExceptions;
+
+namespace tema_student_classs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("introduceti varsta:");
+            int varsta = int.Parse(Console.ReadLine());
+            Console.WriteLine("introduceti numele :");
+            string numele = Console.ReadLine();
+            Student FirstStudent = new Student(numele, varsta);
+            
+            
+            Console.WriteLine(FirstStudent.Info);
+            
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudentExceptions;
+
+
+namespace tema_student_classs
+{
+    class Student
+    {
+        private int age;
+        private string name;
+        private const int min_Age = 18;
+        private const int max_Age = 99;
+        private int mark;
+        public Student()
+        {
+
+        }
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 99)
+                    age = value;
+                else Console.WriteLine("varsta nu e buna");
+
+            }
+        }
+            public int Mark { get; set; }
+        public string Info
+        {
+            get { return name + " " + age; }
+        }
+        
+
+    }
+}
